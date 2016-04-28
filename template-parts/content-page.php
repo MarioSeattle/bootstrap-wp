5a57a287b25a0f7b2dfaaef886990adbf14f4efe
MarioSeattle <marioseattle@live.com>
2016-04-28 14:51:19 -0700

    SHH

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -4,7 +4,7 @@
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package _Hola
+ * @package SSH
  */
 
 ?>
@@ -19,7 +19,7 @@
 			the_content();
 
 			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_hola' ),
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'SSH' ),
 				'after'  => '</div>',
 			) );
 		?>
@@ -30,7 +30,7 @@
 			edit_post_link(
 				sprintf(
 					/* translators: %s: Name of current post */
-					esc_html__( 'Edit %s', '_hola' ),
+					esc_html__( 'Edit %s', 'SSH' ),
 					the_title( '<span class="screen-reader-text">"', '"</span>', false )
 				),
 				'<span class="edit-link">',
